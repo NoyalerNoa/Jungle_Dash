4245814ceb95a9c663abc98d95f760c0db6835a3
michinex0805 <michael.rummer@vcon.at>
2026-05-20 19:04:22 +0200

    Revert "Made the full settings window"

--- jungle-dash-godot/StartScreen.cs
+++ jungle-dash-godot/StartScreen.cs
@@ -3,47 +3,32 @@ using System;
 
 public partial class StartScreen : Control
 {
-	private VBoxContainer mainButtons;
-	private Panel options;
-
+	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// Nodes taken
-		mainButtons = GetNode<VBoxContainer>("MainButtons");
-		options = GetNode<Panel>("Options");
-
-		// make it seeing
-		mainButtons.Visible = true;
-		options.Visible = false;
-
-		// Connect the Buttons
-		GetNode<Button>("MainButtons/StartButton").Pressed += OnStartPressed;
-		GetNode<Button>("MainButtons/OptionsButton").Pressed += OnOptionsPressed;
-		GetNode<Button>("MainButtons/ExitButton").Pressed += OnExitPressed;
-		GetNode<Button>("Options/BackButton").Pressed += OnBackOptionsPressed;
+		GetNode<Button>("StartButton").Pressed += _on_start_pressed;
+		GetNode<Button>("OptionsButton").Pressed += _on_options_pressed;
+		GetNode<Button>("ExitButton").Pressed += _on_exit_pressed;
 	}
 
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
 	// Start the Game
-	private void OnStartPressed()
+	private void _on_start_pressed()
 	{
 		GetTree().ChangeSceneToFile("res://main_scene.tscn");
 	}
-
+	
 	// Options / Settings
-	private void OnOptionsPressed()
+	private void _on_options_pressed()
 	{
-		mainButtons.Visible = false;
-		options.Visible = true;
 	}
 	
-	private void OnBackOptionsPressed()
-	{
-		options.Visible = false;
-		mainButtons.Visible = true;
-	}
-
-	// Leave the Game
-	private void OnExitPressed()
+	// Exit / Leave the Game
+	private void _on_exit_pressed()
 	{
 		GetTree().Quit();
 	}
