6909ce5d67d202791dd11a6f47ac30bf4010820d
NoyalerNoa <noarudisser@gmail.com>
2026-05-27 11:38:15 +0200

    Added a Dash to the Player
    Added a Dash with animation to the Player with variable Dash speed, duration and cooldown.

--- jungle-dash-godot/player/PlayerControls.cs
+++ jungle-dash-godot/player/PlayerControls.cs
@@ -1,16 +1,36 @@
 using Godot;
+using Godot.Collections;
 using System;
 
 public partial class PlayerControls : CharacterBody2D
 {
 	[Export] public float Speed = 300.0f;
+	[Export] public float DashVelocitiy = 1000.0f;
+	[Export] public int MaxDashFrames = 10;
+	[Export] public int CooldownFrames = 10;
+	private int CurrentDashFrames;
 	[Export] public float JumpVelocity = -400.0f;
 	private AnimatedSprite2D animatedSprite; // Von KI
+	public Dictionary<string, bool> abilitys = new Dictionary<string, bool>()
+	{
+		{"Dash", false }
+	};
 
 	public override void _Ready()
 	{
-		animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-	} //Bis hier
+		animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D"); //Bis hier
+		CurrentDashFrames = MaxDashFrames;
+	}
+
+	public Vector2 Dash(Vector2 velocity)
+	{
+		if (animatedSprite.FlipH)
+			velocity.X -= DashVelocitiy;
+		else
+			velocity.X += DashVelocitiy;
+		CurrentDashFrames--;
+		return velocity;
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -37,27 +57,58 @@ public partial class PlayerControls : CharacterBody2D
 
 		velocity.X = direction * Speed;
 
-		if (IsOnFloor())
+		if (Input.IsActionJustPressed("dash") && CurrentDashFrames == MaxDashFrames)
 		{
-			if (direction != 0)
+			velocity = Dash(velocity);
+		}
+
+		if (CurrentDashFrames < MaxDashFrames)
+		{
+			if (CurrentDashFrames > 0)
 			{
-				animatedSprite.Play("run");
+				velocity = Dash(velocity);
 			}
-			else
+			else if (CurrentDashFrames > -CooldownFrames)
 			{
-				animatedSprite.Play("idle");
+				CurrentDashFrames--;
 			}
+		}
+
+		if (CurrentDashFrames + CooldownFrames <= 0)
+		{
+			CurrentDashFrames = MaxDashFrames;
+		}
+
+		if (CurrentDashFrames < MaxDashFrames && CurrentDashFrames > 0)
+		{
+			animatedSprite.Play("dash");
 		}
 
 		else
 		{
-			if (velocity.Y > 0)
+
+			if (IsOnFloor())
 			{
-				animatedSprite.Play("jump");
+				if (direction != 0)
+				{
+					animatedSprite.Play("run");
+				}
+				else
+				{
+					animatedSprite.Play("idle");
+				}
 			}
+
 			else
 			{
-				animatedSprite.Play("fall");
+				if (velocity.Y > 0)
+				{
+					animatedSprite.Play("jump");
+				}
+				else
+				{
+					animatedSprite.Play("fall");
+				}
 			}
 		}
 
