696a19a69188fd41214ce0d15b10fb8da6025680
NoyalerNoa <noarudisser@gmail.com>
2026-06-01 14:20:24 +0200

    Added Walljump and tried fixing the movement

--- jungle-dash-godot/player/PlayerControls.cs
+++ jungle-dash-godot/player/PlayerControls.cs
@@ -5,63 +5,73 @@ using System;
 public partial class PlayerControls : CharacterBody2D
 {
 	[Export] public float Speed = 300.0f;
-	[Export] public float DashVelocitiy = 1000.0f;
+	[Export] public float DashVelocity = 1000.0f;
 	[Export] public int MaxDashFrames = 10;
 	[Export] public int CooldownFrames = 10;
 	private int CurrentDashFrames;
+	[Export] public float VerticalWalljumpVelocity = 1500.0f;
 	[Export] public float JumpVelocity = -400.0f;
-	private AnimatedSprite2D animatedSprite; // Von KI
+
+	// Reibung für das automatische Abbremsen nach dem Dash
+	[Export] public float DashBrakeSpeed = 2000.0f;
+
 	public Dictionary<string, bool> abilitys = new Dictionary<string, bool>()
 	{
-		{"Dash", false }
+		{"Dash", true },
+		{"Walljump", true}
 	};
+	private AnimatedSprite2D animatedSprite;
 
 	public override void _Ready()
 	{
-		animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D"); //Bis hier
+		animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		CurrentDashFrames = MaxDashFrames;
 	}
 
 	public Vector2 Dash(Vector2 velocity)
 	{
 		if (animatedSprite.FlipH)
-			velocity.X -= DashVelocitiy;
+			velocity.X = -DashVelocity;
 		else
-			velocity.X += DashVelocitiy;
+			velocity.X = DashVelocity; // Vereinfacht für konstanten Dash-Schnitt
+
 		CurrentDashFrames--;
+		velocity.Y = 0;
 		return velocity;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
+		float direction = Input.GetAxis("move_left", "move_right");
 
-		// Add the gravity.
+		// Gravitation
 		if (!IsOnFloor())
 		{
 			velocity += GetGravity() * (float)delta;
 		}
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+		// Wandsprung & Sprung
+		if (Input.IsActionJustPressed("jump"))
 		{
-			velocity.Y = JumpVelocity;
+			if (IsOnFloor())
+			{
+				velocity.Y = JumpVelocity;
+			}
+			else if (abilitys["Walljump"] && IsOnWall())
+			{
+				velocity.Y = JumpVelocity;
+				velocity.X = VerticalWalljumpVelocity * GetWallNormal().X;
+			}
 		}
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-
-		// Von Ki weil das automatische nicht ging.
-		float direction = Input.GetAxis("move_left", "move_right");
-
-
-		velocity.X = direction * Speed;
-
-		if (Input.IsActionJustPressed("dash") && CurrentDashFrames == MaxDashFrames)
+		// Dash starten
+		if (Input.IsActionJustPressed("dash") && abilitys["Dash"] && CurrentDashFrames == MaxDashFrames)
 		{
 			velocity = Dash(velocity);
 		}
 
+		// Dash Logik & Abbremsen
 		if (CurrentDashFrames < MaxDashFrames)
 		{
 			if (CurrentDashFrames > 0)
@@ -70,60 +80,54 @@ public partial class PlayerControls : CharacterBody2D
 			}
 			else if (CurrentDashFrames > -CooldownFrames)
 			{
+				// Automatische Abbremsung statt hartem "velocity.X = 0;"
+				velocity.X = Mathf.MoveToward(velocity.X, direction * Speed, DashBrakeSpeed * (float)delta);
 				CurrentDashFrames--;
 			}
 		}
 
+		// Cooldown Reset
 		if (CurrentDashFrames + CooldownFrames <= 0)
 		{
 			CurrentDashFrames = MaxDashFrames;
 		}
 
+		// Animationen & Normale Bewegung
 		if (CurrentDashFrames < MaxDashFrames && CurrentDashFrames > 0)
 		{
 			animatedSprite.Play("dash");
 		}
-
 		else
 		{
+			// Normale Bewegung greift nur, wenn wir nicht mehr aktiv bremsen oder das Tempo schon angepasst ist
+			if (CurrentDashFrames <= 0)
+			{
+				velocity.X = Mathf.MoveToward(velocity.X, direction * Speed, Speed * 10.0f * (float)delta);
+			}
 
 			if (IsOnFloor())
 			{
 				if (direction != 0)
-				{
 					animatedSprite.Play("run");
-				}
 				else
-				{
 					animatedSprite.Play("idle");
-				}
 			}
-
 			else
 			{
 				if (velocity.Y > 0)
-				{
 					animatedSprite.Play("jump");
-				}
 				else
-				{
 					animatedSprite.Play("fall");
-				}
 			}
 		}
 
-
+		// Blickrichtung des Sprites
 		if (direction > 0)
-		{
 			animatedSprite.FlipH = false;
-		}
 		else if (direction < 0)
-		{
 			animatedSprite.FlipH = true;
-		}
 
 		Velocity = velocity;
-		//Bis hier
 		MoveAndSlide();
 	}
 }
